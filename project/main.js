3037d2f96cc982aa203918b2c69aae4f2bd523f9
Kwang Kian Hui <2k1h297@gmail.com>
2021-10-09 00:11:50 +0800

    implement svg animation, about info, about social links, about tabs

--- project/main.js
+++ project/main.js
@@ -1,4 +1,4 @@
-/* ----- Navigation -----*/
+// ----- Navigation -----
 
 (() =>{
     const hamburgerBtn = document.querySelector(".hamburger-btn"),
@@ -10,11 +10,57 @@
     
     function showNavMenu(){
         navMenu.classList.add("open");
+        // bodyScrollingToggle();
     }
 
     function hideNavMenu(){
         navMenu.classList.remove("open");
+        fadeOutEffect();
+        // bodyScrollingToggle();
     }
+
+    function fadeOutEffect(){
+        document.querySelector(".fade-out-effect").classList.add("active");
+        setTimeout(() =>{
+            document.querySelector(".fade-out-effect").classList.remove("active");
+        },300)
+    }
+
+    document.addEventListener("click", (event) => {
+        if(event.target.classList.contains("link-item")){
+            // ensure event.target.hash has a value before overriding default behavior 
+            if(event.target.hash != ""){
+                event.preventDefault();
+                const hash = event.target.hash;
+                console.log(hash);
+                // add and remove active and hide tags respectively for current page
+                document.querySelector(".section.active").classList.add("hide");
+                document.querySelector(".section.active").classList.remove("active");
+                // add and remove active and hide tags respectively for new page
+                document.querySelector(hash).classList.add("active");
+                document.querySelector(hash).classList.remove("hide");
+                
+                navMenu.querySelector(".active").classList.add("outer-shadow","hover-in-shadow");
+                navMenu.querySelector(".active").classList.remove("active","inner-shadow");
+
+                // only if navigation is performed on the menu
+                if(navMenu.classList.contains("open")){
+                    event.target.classList.add("active","inner-shadow");
+                    event.target.classList.remove("outer-shadow","hover-in-shadow");
+                    hideNavMenu();
+                }else{
+                    let navItems = navMenu.querySelectorAll(".link-item");
+                    navItems.forEach((item) => {
+                        if(hash === item.hash){
+                            item.classList.add("active","inner-shadow");
+                            item.classList.remove("outer-shadow","hover-in-shadow");  
+                        }
+                    })
+                    fadeOutEffect();
+                }
+            }
+        }
+    })
 })();
 
 (() => {
